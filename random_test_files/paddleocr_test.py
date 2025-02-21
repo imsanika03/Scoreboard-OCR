7e5578a8b1cdd8ce78d9044ebc97d1425a12eaa9
imsanika03 <imsanika03@gmail.com>
2025-02-20 22:29:57 -0800

    recent update

--- random_test_files/paddleocr_test.py
+++ random_test_files/paddleocr_test.py
@@ -1,15 +1,61 @@
 
 import cv2
+from matplotlib import pyplot as plt
+import numpy as np
 import paddleocr
 from paddleocr import PaddleOCR, draw_ocr
+from PIL import Image
 # Paddleocr supports Chinese, English, French, German, Korean and Japanese.
 # You can set the parameter `lang` as `ch`, `en`, `french`, `german`, `korean`, `japan`
 # to switch the language model in order.
-ocr = PaddleOCR(use_angle_cls=True, lang='en') # need to run only once to download and load model into memory
-img_path = "/Users/sanikabharvirkar/Documents/pprlastshot/match01_paddle_output/frame_0739.jpg"
+from PIL import Image
+
+ocr = PaddleOCR(use_angle_cls=True, lang='en', ocr_version='PP-OCRv4', use_space_char=True) # need to run only once to download and load model into memory
+img_path = "/Users/sanikabharvirkar/Documents/pprlastshot/random_test_files/inner_box_final.png"
 img = cv2.imread(img_path)
-inverted_image = cv2.bitwise_not(img)
-cv2.imwrite(img_path, inverted_image)
+# Check if the image was loaded successfully
+
+
+def make_non_black_pixels_white(image_path, output_path):
+    # Open the image
+    image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Create a mask where black pixels remain (thresholding)
+    mask = gray < 100  # Adjust threshold if needed
+
+    # Create a white canvas
+    output = np.ones_like(image) * 255  # White background
+
+    # Keep only black pixels
+    output[mask] = image[mask]
+    cv2.imwrite(output_path, output)
+    # Return the processed image path
+
+
+# Example usage:
+make_non_black_pixels_white(img_path, "/Users/sanikabharvirkar/Documents/pprlastshot/random_test_files/inner_box_final_b&w.png")
+
+img = cv2.imread("/Users/sanikabharvirkar/Documents/pprlastshot/random_test_files/outer_box_final2.png")
+img = cv2.resize(img, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+scale_factor = 2  # Magnify by 2x
+
+# Get new dimensions
+height, width = img.shape[:2]
+new_dimensions = (int(width * scale_factor), int(height * scale_factor))
+
+# Resize the image
+magnified_image = cv2.resize(img, new_dimensions, interpolation=cv2.INTER_LINEAR)
+
+
+# invert image since paddleocr trained dataset doesnt include numbers with black background
+inverted_image = cv2.bitwise_not(magnified_image)
+
+plt.imshow(inverted_image)
+plt.axis('off')
+plt.show()
+
 #img = cv2.resize(img, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
 #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #scale_factor = 1.0  # Magnify by 2x
@@ -31,10 +77,10 @@ cv2.imwrite(img_path, inverted_image)
 
 #cv2.imwrite(img_path, magnified_image)
 
-result = ocr.ocr(img_path, cls=True)
-print(len(result))
+result = ocr.ocr(inverted_image, cls=True)
+
 for line in result:
-    print(len(line))
+
     for text in line: 
         print(text[1][0])
 
--- src/ScoreboardIdentifier.py
+++ src/ScoreboardIdentifier.py
@@ -1,4 +1,7 @@
 import ssl
+
+from matplotlib import pyplot as plt
+import numpy as np
 ssl._create_default_https_context = ssl._create_unverified_context
 
 import cv2
@@ -32,10 +35,14 @@ class TextIdentifier():
         if valid_board is not None: 
 
             # pre process image for better ocr results 
-            self.ocr_image_preprocessing(valid_board, save_proprocessed=True)
-
-            text_result = self.ocr.ocr(self.image_path, cls=True)
-            return True, text_result
+            ok, full_score_box = self.ocr_image_preprocessing(valid_board, save_proprocessed=False)
+      
+            if not ok: 
+                return False, "Full box not found"
+            
+            full_ocr = self.ocr.ocr(full_score_box, cls=True)
+            
+            return True, full_ocr
         
         else: 
             return False, "Board not found, skip frame"
@@ -69,34 +76,209 @@ class TextIdentifier():
             return None
         
     def ocr_image_preprocessing(self, img, save_proprocessed=False): 
+
+        if save_proprocessed:
+            cv2.imwrite(self.image_path + "cropped.png", img)
+
+        full_score_box = crop_full_score_box(img)
+
+        if full_score_box is None or full_score_box.size == 0: 
+            return False, None
         
-        img = cv2.resize(img, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        scale_factor = 3  # Magnify by 2x
 
-        # Get new dimensions
-        height, width = img.shape[:2]
-        new_dimensions = (int(width * scale_factor), int(height * scale_factor))
-
-        # Resize the image
-        magnified_image = cv2.resize(img, new_dimensions, interpolation=cv2.INTER_LINEAR)
-
-        #crop the noise from top and bottom
-        height, width = magnified_image.shape[:2]
-        top_crop = height // 10  # 1/8th of the height
-        bottom_crop = height - top_crop
-        magnified_image = magnified_image[top_crop:bottom_crop, :]
-
-        # invert image since paddleocr trained dataset doesnt include numbers with black background
-        inverted_image = cv2.bitwise_not(magnified_image)
+        full_score_box_outer = preprocess_outer_box(full_score_box, inner_box=False)
 
         if save_proprocessed: 
 
-            cv2.imwrite(self.image_path, inverted_image)
-            return inverted_image
+            cv2.imwrite(self.image_path + "full_score_box.png", full_score_box_outer)
+        
+        return True, full_score_box_outer
 
-        return magnified_image
 
     
     def update_image(self, image_path): 
         self.image_path = image_path
+
+def crop_full_score_box(frame): 
+    
+    original_image = frame.copy()
+    image = frame.copy()
+
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+                               cv2.THRESH_BINARY_INV, 11, 2)
+
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    bounding_boxes = [cv2.boundingRect(cnt) for cnt in contours]
+
+
+    # final bounding box
+    if bounding_boxes:
+
+        leftmost = min(bounding_boxes, key=lambda b: b[0])[0]
+        rightmost = max(bounding_boxes, key=lambda b: b[0] + b[2])[0] + max(bounding_boxes, key=lambda b: b[0] + b[2])[2]
+        top = min(bounding_boxes, key=lambda b: b[1])[1]
+        bottom = max(bounding_boxes, key=lambda b: b[1] + b[3])[1] + max(bounding_boxes, key=lambda b: b[1] + b[3])[3]
+        
+ 
+        padding = 3  # padding to exclude bounding box lines
+        leftmost += padding
+        rightmost -= padding
+        top += padding
+        bottom -= padding
+        
+        cropped = original_image[top:bottom, leftmost:rightmost]
+        return cropped
+
+def crop_inner_score_box(frame): 
+    original_image = frame.copy()
+    image = original_image.copy()
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    _, thresh = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY_INV)
+
+
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    bounding_boxes = [cv2.boundingRect(cnt) for cnt in contours]
+
+    # Determine the final bounding box
+    if bounding_boxes:
+
+        leftmost_box = bounding_boxes[0]
+        rightmost_box = bounding_boxes[0]
+        top = bounding_boxes[0][1]
+        bottom = bounding_boxes[0][1] + bounding_boxes[0][3]
+        
+        for box in bounding_boxes:
+            x, y, w, h = box
+            if x < leftmost_box[0]:
+                leftmost_box = box
+            if x + w > rightmost_box[0] + rightmost_box[2]:
+                rightmost_box = box
+            if y < top:
+                top = y
+            if y + h > bottom:
+                bottom = y + h
+        
+        left_x = leftmost_box[0] + leftmost_box[2]  # right line of left box
+        right_x = rightmost_box[0]  # left line of right box
+        
+
+        cropped = original_image[top:bottom, left_x:right_x]
+        return cropped
+
+def preprocess_inner_box(frame): 
+
+    frame = make_non_black_pixels_white(frame)
+
+    img = cv2.resize(frame, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    scale_factor = 2  # Magnify by 2x
+
+    height, width = img.shape[:2]
+    new_dimensions = (int(width * scale_factor), int(height * scale_factor))
+
+    magnified_image = cv2.resize(img, new_dimensions, interpolation=cv2.INTER_LINEAR)
+    return magnified_image
+
+def preprocess_outer_box(frame, inner_box=False): 
+    
+    img = cv2.resize(frame, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    scale_factor = 2  # Magnify by 2x
+
+    height, width = img.shape[:2]
+    new_dimensions = (int(width * scale_factor), int(height * scale_factor))
+
+    magnified_image = cv2.resize(img, new_dimensions, interpolation=cv2.INTER_LINEAR)
+
+    if inner_box: 
+
+        _, width = magnified_image.shape[:2]
+
+        # Compute x-coordinates for inner score box
+        x1 = int((8.1 / 10.3) * width)
+        x2 = int((9.3 / 10.3) * width)
+
+        magnified_image[:, x1:x2] = make_non_black_pixels_white(magnified_image[:, x1:x2])
+    
+        magnified_image[:, 0:x1] = cv2.bitwise_not(magnified_image[:, 0:x1])
+        magnified_image[:, x2:] = cv2.bitwise_not(magnified_image[:, x2:])
+    
+        return magnified_image
+    else: 
+        inverted_image = cv2.bitwise_not(magnified_image)
+        return inverted_image
+
+def make_non_black_pixels_white(frame):
+
+    image = frame
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    mask = gray < 100  # grey thresh
+
+    output = np.ones_like(image) * 255  # White background
+
+    # only black pixels
+    output[mask] = image[mask]
+    return output
+
+def crop_outer_score_boxes(frame): 
+    
+    original_image = frame.copy()
+    image = original_image.copy()
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    _, thresh = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY_INV)
+
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    bounding_boxes = [cv2.boundingRect(cnt) for cnt in contours]
+
+    if bounding_boxes:
+        leftmost = min(bounding_boxes, key=lambda b: b[0])[0]
+        rightmost = max(bounding_boxes, key=lambda b: b[0] + b[2])[0] + max(bounding_boxes, key=lambda b: b[0] + b[2])[2]
+        top = min(bounding_boxes, key=lambda b: b[1])[1]
+        bottom = max(bounding_boxes, key=lambda b: b[1] + b[3])[1] + max(bounding_boxes, key=lambda b: b[1] + b[3])[3]
+        
+
+        # find inner box: 
+        leftmost_box = bounding_boxes[0]
+        rightmost_box = bounding_boxes[0]
+        top_line = bounding_boxes[0][1]
+        bottom_line = bounding_boxes[0][1] + bounding_boxes[0][3]
+        
+        for box in bounding_boxes:
+            x, y, w, h = box
+            if x < leftmost_box[0]:
+                leftmost_box = box
+            if x + w > rightmost_box[0] + rightmost_box[2]:
+                rightmost_box = box
+            if y < top:
+                top = y
+            if y + h > bottom:
+                bottom = y + h
+        
+        left_x = leftmost_box[0] + leftmost_box[2]  
+        right_x = rightmost_box[0] 
+
+        if original_image.shape[2] == 4:  
+            original_image[top:bottom, left_x:right_x] = [0, 0, 0, 255]
+        elif original_image.shape[2] == 3:  
+            original_image[top:bottom, left_x:right_x] = [0, 0, 0] 
+    
+        # remove extra noise
+        padding = 3  
+        leftmost += padding
+        rightmost -= padding
+        top += padding
+        bottom -= padding
+        
+        cropped = original_image[top:bottom, leftmost:rightmost]
+        return cropped
